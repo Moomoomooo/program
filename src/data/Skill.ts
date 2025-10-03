0f1594b599fb2c27d74ea4436b958201ea938a5b
Moomoomooo <83972202+Moomoomooo@users.noreply.github.com>
2025-10-03 11:22:06 -0400

    Update Skill.ts

--- src/data/Skill.ts
+++ src/data/Skill.ts
@@ -1,6 +1,14 @@
 import { SkillFlags } from '#enums/SkillFlags.ts';
-import {SkillTargets } from '#enums/SkillTargets.ts';
+import { SkillTargets } from '#enums/SkillTargets.ts';
+import { SkillPhases } from '#enums/SkillPhases.ts';
 
 export class Skill {
+  name: string;
+  cooldown: number;
+  time: number;
+  phase: SkillPhases;
+  targets: SkillTargets;
+  flags: SkillFlags[];
+
   
 }
