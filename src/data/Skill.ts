ac072969b33dc97cdc50ba32653fb379c3ef501d
Moomoomooo <83972202+Moomoomooo@users.noreply.github.com>
2025-10-03 12:00:42 -0400

    Update Skill.ts

--- src/data/Skill.ts
+++ src/data/Skill.ts
@@ -1,14 +1,26 @@
 import { SkillFlags } from '#enums/SkillFlags.ts';
 import { SkillTargets } from '#enums/SkillTargets.ts';
 import { SkillPhases } from '#enums/SkillPhases.ts';
+import { Entity } from '#enums/Entity.ts';
 
 export class Skill {
-  name: string;
-  cooldown: number;
+  readonly name: string;
+  readonly cooldown: number;
   time: number;
-  phase: SkillPhases;
-  targets: SkillTargets;
-  flags: SkillFlags[];
+  ready: boolean;
+  readonly phase: SkillPhases;
+  readonly targets: SkillTargets;
+  readonly flags: SkillFlags[];
+  effect: (target) => void;
 
-  
+  constructor(name: string, cooldown: number, phase: SkillPhase, targets: SkillTargets, flags: SkillFlags[]) {
+    
+  }
+
+  public cool(): void {
+    this.time++;
+    if (this.time >= this.cooldown) {
+      this.ready = true;
+    }
+  }
 }
