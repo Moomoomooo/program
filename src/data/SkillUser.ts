049f9dc9a18d46fb21aa0ca07a6a980ca7418d7d
Moomoomooo <83972202+Moomoomooo@users.noreply.github.com>
2025-10-03 10:06:49 -0400

    Update SkillUser.ts

--- src/data/SkillUser.ts
+++ src/data/SkillUser.ts
@@ -1,9 +1,9 @@
 import { Skill } from '#data/Skill.ts';
 import { Die } from '#data/Die.ts';
 
-export abstract class SkillUser {
+export interface SkillUser {
   name: string;
-  dice: Die[];
   stats: number[]; // maxHp, rollMod, spellMod, initiative, armor, statusResist
 
+  
 }
