74d9bf38ef79e3bba0d0f44c0014822a5067bf96
Moomoomooo <83972202+Moomoomooo@users.noreply.github.com>
2025-10-07 09:25:00 -0400

    Update Entity.ts

--- src/data/Entity.ts
+++ src/data/Entity.ts
@@ -3,10 +3,12 @@ import { SkillUser } from '#data/SkillUser.ts';
 export abstract class Entity extends SkillUser {
   
   tempStats: number[];
+  hp: number;
 
   constructor(name: string, stats: number[]) {
     super();
-    tempStats = [0, 0, 0, 0, 0, 0];
+    this.tempStats = [0, 0, 0, 0, 0, 0];
+    this.hp = this.stats[0];
     
   }
 
