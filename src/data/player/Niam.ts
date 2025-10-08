21c807ce81f806691405c692c7be84470a813231
Moomoomooo <83972202+Moomoomooo@users.noreply.github.com>
2025-10-08 08:52:51 -0400

    Update Niam.ts

--- src/data/player/Niam.ts
+++ src/data/player/Niam.ts
@@ -1,4 +1,4 @@
 import { Entity } from '#data/Entity.ts';
 import { PlayerJobs } from '#enums/PlayerJobs.ts'
 
-export const Niam: Entity = new Entity("Niam", )
+export const Niam: Entity = new Entity("Niam", [10, 0, 0, 1, 5], ;
