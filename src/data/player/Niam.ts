07f741ed782617e7590277c2f8cd6c5f35a9f00e
Moomoomooo <83972202+Moomoomooo@users.noreply.github.com>
2025-10-08 09:08:23 -0400

    Update Niam.ts

--- src/data/player/Niam.ts
+++ src/data/player/Niam.ts
@@ -1,7 +1,13 @@
 import { Entity } from '#data/Entity.ts';
+import { SkillFlags } from '#enums/SkillFlags.ts';
+import { SkillPhases } from '#enums/SkillPhases.ts';
 import { SkillTargets } from '#enums/SkillTargets.ts';
 
 export const Niam: Entity = new Entity("Niam", [10, 0, 0, 1, 5], "d10");
 
-Niam.addSkill("power attack", 3, )
-Niam.addSkill("fire bolt", 2, )
+Niam.addSkill("power attack", 3, [SkillFlags.PHYSICAL, SkillFlags.DAMAGING], SkillPhases.ATTACK, SkillTargets.ENEMY, (target: Entity): void => {
+  
+});
+Niam.addSkill("fire bolt", 2, [SkillFlags.SPELL, SkillFlags.FIRE, SkillFlags.DAMAGING], SkillPhases.ATTACK, SkillTargets.ENEMY, (target: Entity): void => {
+  
+});
