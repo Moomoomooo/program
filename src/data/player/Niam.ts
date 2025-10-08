901502af47f0db8d8941e10ecd1727b988259f23
Moomoomooo <83972202+Moomoomooo@users.noreply.github.com>
2025-10-08 09:58:57 -0400

    Update Niam.ts

--- src/data/player/Niam.ts
+++ src/data/player/Niam.ts
@@ -3,11 +3,11 @@ import { SkillFlags } from '#enums/SkillFlags.ts';
 import { SkillPhases } from '#enums/SkillPhases.ts';
 import { SkillTargets } from '#enums/SkillTargets.ts';
 
-export const Niam: Entity = new Entity("Niam", [10, 0, 0, 1, 5], "d10");
+export const Niam: Entity = new Entity("Niam", [10, 0, 0, 1, 5], 10);
 
 Niam.addSkill("power attack", 3, [SkillFlags.PHYSICAL, SkillFlags.DAMAGING], SkillPhases.ATTACK, SkillTargets.ENEMY, (): number => {
-  return 
+  return 2 * master.calcVariableAttackPower(0.85) + master.stats[1];
 });
 Niam.addSkill("fire bolt", 2, [SkillFlags.SPELL, SkillFlags.FIRE, SkillFlags.DAMAGING], SkillPhases.ACTIVATE, SkillTargets.ENEMY, (): number => {
-  return master.dice.pool[0] + master.stats[2];
+  return 0.25 * master.calcVariableAttackPower(0.75) + master.stats[2];
 });
