11633a57a944a0fcaacac83fc69945b138bb8066
Moomoomooo <83972202+Moomoomooo@users.noreply.github.com>
2025-10-08 10:27:37 -0400

    Update Niam.ts

--- src/data/player/Niam.ts
+++ src/data/player/Niam.ts
@@ -2,7 +2,8 @@ import { Entity } from '#data/Entity.ts';
 import { SkillFlags } from '#enums/SkillFlags.ts';
 import { SkillPhases } from '#enums/SkillPhases.ts';
 import { SkillTargets } from '#enums/SkillTargets.ts';
-import { levelupData } 
+import { levelUpData } from '#player/LevelUpData.ts';
+import { Skill } from '#data/Skill.ts';
 
 export const niam: Entity = new Entity("Niam", [10, 0, 0, 1, 5], 10);
 niam.level = 1;
@@ -10,7 +11,11 @@ niam.level = 1;
 niam.levelUp = (): void => {
   this.level++;
 
-  
+  for (const data: [number, Skill] of levelUpData) {
+    if (data[0] === this.level) {
+      this.skills.push(data[1]);
+    }
+  }
 }
 
 niam.addSkill("power attack", 3, [SkillFlags.PHYSICAL, SkillFlags.DAMAGING], SkillPhases.ATTACK, SkillTargets.ENEMY, (): number => {
