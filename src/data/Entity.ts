9fdba2353fe316f408f74fdb21350097dde916da
Moomoomooo <83972202+Moomoomooo@users.noreply.github.com>
2025-10-08 08:48:13 -0400

    Update Entity.ts

--- src/data/Entity.ts
+++ src/data/Entity.ts
@@ -3,15 +3,19 @@ import { DicePool } from '#DicePool.ts';
 
 export abstract class Entity extends SkillUser {
   
-  tempStats: number[];
+  tempStats: number[]; // total change in base stats
+  this.tempDmgResists: // total changes in dmg resistances
   hp: number;
 
   constructor(name: string, stats: number[], dice: string) {
     super();
-    this.tempStats = [0, 0, 0, 0, 0, 0];
+    this.tempStatChanges = new Array(6);
+    this.tempDmgResists = new Array(8);
     this.hp = this.stats[0];
-    
   }
 
+  public calcRawBaseAttackDmg(): number {
+    return this.dice.rollAll() + this.
+  }
   
 }
