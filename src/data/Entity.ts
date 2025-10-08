f4d89774b291caa513b58edccc9d1ffbe0dfb18f
Moomoomooo <83972202+Moomoomooo@users.noreply.github.com>
2025-10-08 07:53:22 -0400

    Update Entity.ts

--- src/data/Entity.ts
+++ src/data/Entity.ts
@@ -5,13 +5,11 @@ export abstract class Entity extends SkillUser {
   
   tempStats: number[];
   hp: number;
-  dice: DicePool;
 
   constructor(name: string, stats: number[], dice: string) {
     super();
     this.tempStats = [0, 0, 0, 0, 0, 0];
     this.hp = this.stats[0];
-    this.dice = new DicePool(dice);
     
   }
 
