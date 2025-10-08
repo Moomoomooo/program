8277c888910a731dbfb410637b40db169aaecd29
Moomoomooo <83972202+Moomoomooo@users.noreply.github.com>
2025-10-08 08:14:18 -0400

    Update Monster.ts

--- src/data/monster/Monster.ts
+++ src/data/monster/Monster.ts
@@ -9,9 +9,11 @@ export class Monster extends Entity {
 
 /* new Monster("Cacophony of the Holy")
 
-cacophony.addDmgResist(DmgResistance.PHYSICAL, 0.75)
+cacophony.addDmgResist(DmgResistance.PHYSICAL, 0.5)
 cacophony.addDmgResist(DmgResistance.FIRE, 0.75)
-cacophony.addDmgResist(DmgResistance.ICE, 0.75
+cacophony.addDmgResist(DmgResistance.ICE, 0.75)
+cacophony.addDmgResist(DmgResistance.LIGHTNING, 0.75)
+
 cacophony.addDmgResist(DmgResistance.HOLY, 1);
 
 cacophony.addSkill("Song of the Secret", 3, SkillTarget.ALL_OTHERS, SkillPhase.ATTACK, [SkillFlags.HOLY, SkillFlags.DEBUFF, SKillFlags.DAMAGING], (target: Entity) => {
