1cbe4c6578004b5f9d0dbc8ca69419f1704ca948
Moomoomooo <83972202+Moomoomooo@users.noreply.github.com>
2025-10-08 07:55:00 -0400

    Update SkillUser.ts

--- src/data/SkillUser.ts
+++ src/data/SkillUser.ts
@@ -31,7 +31,7 @@ export abstract class SkillUser {
   }
   
   
-  public addSkill(name: string, cooldown: number, targets: SkillTargets, phase: SkillPhases, flags: SkillFlags[] ,effect: (target) => void): void {
+  public addSkill(name: string, cooldown: number, targets: SkillTargets, phase: SkillPhases, flags: SkillFlags[] ,effect: (target: Entity) => void): void {
 
     const s = {
       name: name,
