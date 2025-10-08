276a62526d2b23c838911f7869161a38806d4997
Moomoomooo <83972202+Moomoomooo@users.noreply.github.com>
2025-10-08 11:32:00 -0400

    Update Ally.ts

--- src/data/allies/Ally.ts
+++ src/data/allies/Ally.ts
@@ -4,21 +4,23 @@ import { Skill } from "#types/Skill.ts';
 export class Ally extends Entity {
   level: number;
   baseAttackPower: number;
+  baseStats: number[];
   skillData: [number, skill][];
-  powerScale: (lv: number, power: number) => number;
+  powerScale: (lv: number, power: number, bStats: number[]) => number;
   
   
   constructor(name: string, stats: number[], attackPower: number, level: number, powerscale: (lv: number, power: number) => number) {
     super();
     this.baseAttackPower = attackPower;
+    this.baseStats = stats[];
     this.level = level;
     this.skillData = [];
     
-    this.rescaleAttackPower();
+    this.rescaleAlly();
   }
 
-  public rescaleAttackPower() {
-    this.powerscale(this.level, this.baseAttackPower);
+  public rescaleAlly() {
+    this.powerscale(this.level, this.baseAttackPower, this.baseStats);
   }
 
 }
