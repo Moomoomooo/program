55a174edb82a28814f7d0b9d4a07efce88b71797
Moomoomooo <83972202+Moomoomooo@users.noreply.github.com>
2025-10-08 10:15:04 -0400

    Update Niam.ts

--- src/data/player/Niam.ts
+++ src/data/player/Niam.ts
@@ -2,12 +2,20 @@ import { Entity } from '#data/Entity.ts';
 import { SkillFlags } from '#enums/SkillFlags.ts';
 import { SkillPhases } from '#enums/SkillPhases.ts';
 import { SkillTargets } from '#enums/SkillTargets.ts';
+import { levelupData } 
 
-export const Niam: Entity = new Entity("Niam", [10, 0, 0, 1, 5], 10);
+export const niam: Entity = new Entity("Niam", [10, 0, 0, 1, 5], 10);
+niam.level = 1;
 
-Niam.addSkill("power attack", 3, [SkillFlags.PHYSICAL, SkillFlags.DAMAGING], SkillPhases.ATTACK, SkillTargets.ENEMY, (): number => {
+niam.levelUp = (): void => {
+  this.level++;
+
+  
+}
+
+niam.addSkill("power attack", 3, [SkillFlags.PHYSICAL, SkillFlags.DAMAGING], SkillPhases.ATTACK, SkillTargets.ENEMY, (): number => {
   return 2 * master.calcVariableAttackPower(0.85) + master.stats[1];
 });
-Niam.addSkill("fire bolt", 2, [SkillFlags.SPELL, SkillFlags.FIRE, SkillFlags.DAMAGING], SkillPhases.ACTIVATE, SkillTargets.ENEMY, (): number => {
+niam.addSkill("fire bolt", 2, [SkillFlags.SPELL, SkillFlags.FIRE, SkillFlags.DAMAGING], SkillPhases.ACTIVATE, SkillTargets.ENEMY, (): number => {
   return 0.25 * master.calcVariableAttackPower(0.75) + master.stats[2];
 });
