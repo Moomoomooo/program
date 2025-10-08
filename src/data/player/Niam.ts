97e901d74cbe5dffc82f3eea56b45d249ac93cf5
Moomoomooo <83972202+Moomoomooo@users.noreply.github.com>
2025-10-08 08:58:38 -0400

    Update Niam.ts

--- src/data/player/Niam.ts
+++ src/data/player/Niam.ts
@@ -2,3 +2,6 @@ import { Entity } from '#data/Entity.ts';
 import { PlayerJobs } from '#enums/PlayerJobs.ts'
 
 export const Niam: Entity = new Entity("Niam", [10, 0, 0, 1, 5], "d10");
+
+Niam.addSkill("power attack", 3, )
+Niam.addSkill("fire bolt", 2, )
