19f9ff926eafc7ca1347f426f79b205aece2bd1d
Moomoomooo <83972202+Moomoomooo@users.noreply.github.com>
2025-10-08 07:51:16 -0400

    Update Monster.ts

--- src/data/monster/Monster.ts
+++ src/data/monster/Monster.ts
@@ -1,6 +1,14 @@
 import { Entity } from '#data/Entity.ts';
 import { MonsterSpecies } from '#data/Entity.ts';
+import { Skill } from '#data/Skill.ts';
 
 export class Monster extends Entity {
-  
+
 }
+
+
+/* new Monster("Cacophony of the Holy")
+
+cacophony.addSkill("Song of the Secret", 
+
+*/
