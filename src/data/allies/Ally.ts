bb79ba97e6d56701039ec2bbb6178510f8c9cacc
Moomoomooo <83972202+Moomoomooo@users.noreply.github.com>
2025-10-09 08:46:21 -0400

    Update Ally.ts

--- src/data/allies/Ally.ts
+++ src/data/allies/Ally.ts
@@ -2,6 +2,7 @@ import { Entity } from '#data/Entity.ts';
 import { Skill } from '#types/Skill.ts';
 import { LevelUpSkill } from '#types/LevelUpSkill.ts';
 import { RegionId } from '#enums/RegionId.ts';
+import { AllyId } from '#enums/AllyId.ts';
   
 export class Ally extends Entity {
   level: number;
@@ -9,12 +10,14 @@ export class Ally extends Entity {
   readonly baseStats: number[];
   skillData: LevelUpSkill[];
   powerScale: () => number;
+  allyId: AllyId;
   location: RegionId;
   
   
-  constructor(name: string, stats: number[], attackPower: number, level: number, powerScale: () => number) {
+  constructor(name: string, allyId: AllyId, stats: number[], attackPower: number, level: number, powerScale: () => number) {
     super();
     this.baseAttackPower = attackPower;
+    this.allyId = allyId;
     this.baseStats = stats[];
     this.level = level;
     this.skillData = [];
