c2f14b70cd7331db390b3013c13406c85b9120d4
Moomoomooo <83972202+Moomoomooo@users.noreply.github.com>
2025-10-09 08:44:50 -0400

    Update NPC.ts

--- src/npcs/NPC.ts
+++ src/npcs/NPC.ts
@@ -1,1 +1,20 @@
+import { AllyId } from '#enums/AllyId.ts'
 
+export class NPC {
+  allyId?: AllyId;
+
+  constructor(allyId?: AllyId) {
+    if (allyId !== undefined) {
+      this.allyId = allyId;
+    }
+  }
+
+  public isAlly(): boolean {
+    if (this.allyId !== undefined) {
+      return true;
+    }
+    return false;
+  }
+
+  
+}
