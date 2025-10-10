d335936f7269c4ee4a21fbe2a409ca922e84abfb
Moomoomooo <83972202+Moomoomooo@users.noreply.github.com>
2025-10-10 08:01:02 -0400

    Update Fight.ts

--- src/battle/Fight.ts
+++ src/battle/Fight.ts
@@ -1,9 +1,11 @@
 import { Entity } from '#data/Entity.ts';
 import { niam } from '#data/Niam.ts';
+import { Skill } from '#types/Skill.ts';
 
-export const fight = {
-  unassignedDamage: 0,
-
-  
+export class Fight {
+  waves: number;
   
 }
+  
+  
+  
